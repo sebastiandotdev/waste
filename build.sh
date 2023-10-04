e7db9bd4ddc761b5e011db15d05f3425ddf26607
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-10-04 07:46:53 -0500

    chore: 📌 Change name to deploy

--- build.sh
+++ build.sh
@@ -25,9 +25,13 @@ go version
 # install Hugo
 echo "Installing Hugo $HUGO_VERSION..."
 curl -sSOL https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
+
 tar -xzf hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
+
 mv hugo /usr/local/bin/
+
 rm -rf hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
+
 hugo version
 
 # install dependencies
